2b1a8994403c369ff6e832714ccdec818099331b
Soumya Ranjan Sahoo <soumyaranjansahoo2031@gmail.com>
2025-11-13 14:50:34 +0530

    learning and exploring

--- Java Basics/src/day7/FunctionalOperationsDemo.java
+++ Java Basics/src/day7/FunctionalOperationsDemo.java
@@ -1,5 +1,20 @@
 package day7;
 
-public class FunctionalOperationsDemo {
+import java.util.Optional;
 
+public class FunctionalOperationsDemo {
+    static void main(String[] args) {
+//        Optional<String> name = Optional.of(null);
+        Optional<String> nullName = Optional.ofNullable("master");
+        System.out.println(nullName);
+
+        //ifPresent
+         nullName.ifPresent(name -> {
+            System.out.println(name);
+        });
+
+         //map()
+         Optional<String> upperCase = nullName.map(String::toUpperCase);
+        System.out.println(upperCase.orElse("default"));
+    }
 }
